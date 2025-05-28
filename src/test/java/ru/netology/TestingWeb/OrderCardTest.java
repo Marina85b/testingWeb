15a540c42e3546835b124404b80a4d8c154f94e9
Neto Marina <koshkalisa2018@yandex.ru>
2025-05-28 20:51:15 +0300

    add README

--- src/test/java/ru/netology/TestingWeb/OrderCardTest.java
+++ src/test/java/ru/netology/TestingWeb/OrderCardTest.java
@@ -23,7 +23,7 @@ public class OrderCardTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-        driver.get("http://localhost:9999");
+        driver.get("http://localhost:7777");
     }
 
     @AfterEach
@@ -34,7 +34,7 @@ public class OrderCardTest {
 
     @Test
     void shouldTestPositive() {
-        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Павел Пестель");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Сергей Пестель");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79217931826");
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.cssSelector("button")).click();
@@ -45,7 +45,7 @@ public class OrderCardTest {
 
     @Test
     void shouldTestPositivResultDoubleSurname() {
-        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Муравьев-Апостол Сергей");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Жуков-Апостол Сергей");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79217951843");
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.cssSelector("button")).click();
@@ -56,7 +56,7 @@ public class OrderCardTest {
 
     @Test
     void shouldTestPositivResultDoubleName() {
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Лягушкин Иван-Царевичь");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Квакушкин Иван-Царевичь");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79770001155");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.className("button")).click();
